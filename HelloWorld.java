600c94f3de077df1f00b36b7a6740c6f26a8e443
Sakae Watanabe <meltdownlv@gmail.com>
2021-03-17 16:38:31 -0700

    Completed first module

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World! It's me Kathryn!");
+
+		System.out.println("Hello World! It's me Sakae! and I love Tacos!");
 	}
 }
