4c554398fa908489ab2f678a1a4b691c1ca6e407
Sakae Watanabe <meltdownlv@gmail.com>
2021-03-17 17:14:23 -0700

    Completed chapter 3 challenge

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,33 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        String question = "What is the meaning of life?";
+        String choiceOne = "1 - Get rich";
+        String choiceTwo = "2 - Die trying to do number 1";
+        String choiceThree = "3 - Spend time with family";
 
-        String correctAnswer = choiceTwo;
+        String correctAnswer = choiceThree;
 
         // Write a print statement asking the question
+        System.out.println(question);
         // Write a print statement giving the answer choices
+        System.out.println(choiceOne);
+        System.out.println(choiceTwo);
+        System.out.println(choiceThree);
 
         // Have the user input an answer
+        System.out.println("What say you?");
+        Scanner answer = new Scanner(System.in);
         // Retrieve the user's input
-
+        int userChoice = answer.nextInt();
         // If the user's input matches the correctAnswer...
-        // then the user is correct and we want to print out a congrats message to the user.
-
-        // If the user's input does not match the correctAnswer...
-        // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
+        if (userChoice == 3) {
+            System.out.println("That's right! Money isn't everything.");
+        } else {
+            System.out.println("You have much to learn grasshopper.");
+        }
 
     }
 
